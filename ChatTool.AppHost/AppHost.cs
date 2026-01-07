3275c4025ec891cb79b5709032b987e593e67e7d
Michael Stutz <michael.stutz@bbv.ch>
2026-01-07 14:59:39 +0100

    added Aspire Project for overview

---
+++ ChatTool.AppHost/AppHost.cs
@@ -0,0 +1,10 @@
+using Projects;
+
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+IResourceBuilder<ProjectResource> server = builder.AddProject<ChatTool_Server>("server");
+
+IResourceBuilder<ProjectResource> client = builder.AddProject<ChatTool_Client>("client")
+    .WithReference(server);
+
+builder.Build().Run();
